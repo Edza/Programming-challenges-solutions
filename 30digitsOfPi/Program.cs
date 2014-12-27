cdd1b73f046be8d36577b57b1f4496915c9aee39
edza <edexdev@gmail.com>
2014-12-27 19:26:16 +0200

    initial commit

---
+++ 30digitsOfPi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _30digitsOfPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Izmantosim viduslaiku indieša Madhava formulu
+            // Piezīme: tehniski runājot tikai pirmās zīmes sakrīt precīzi, jo šis algoritms nav perfekts
+            // un decimal atbalsta 28-29 precīzas zīmes nevis 30 ;)
+
+            Decimal approxPi = 0;
+            for (int k = 0; k < 10000000; k++)
+            {
+                var value = Math.Pow((-1 / 3.0), k) / (2 * k + 1);
+                approxPi += (Decimal)value;
+            }
+
+            approxPi *= (Decimal)Math.Sqrt(12);
+
+            var _30hashes = new string('#', 30);
+
+            Console.WriteLine(approxPi.ToString("#." + _30hashes));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RandomPassGen/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPassGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int numChars;
+
+                Console.WriteLine("Cik ciparu parolei? (-1 lai izietu)");
+
+                try
+                {
+                    numChars = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Nepareizi ievaddati!");
+                    continue;
+                }
+
+                if (numChars == -1)
+                    break;
+
+                var validChars = "1234567890!@#$%^&*()";
+                validChars += "QWERTYUIOPASDFGHJKLZXCVBNM";
+                validChars += "QWERTYUIOPASDFGHJKLZXCVBNM".ToLower();
+
+                var validCharsSeperated = validChars.ToCharArray();
+
+                string output = "";
+
+                Random r = new Random();
+                int max = validCharsSeperated.Count() - 1;
+
+                for (int i = 0; i < numChars; i++)
+                {
+                    output += validCharsSeperated[r.Next(0, max)];
+                }
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
