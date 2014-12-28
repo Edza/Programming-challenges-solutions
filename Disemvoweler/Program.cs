162fcedc1641d5b257d83f485d0dd1a93f39aade
edza <edexdev@gmail.com>
2014-12-28 19:45:46 +0200

    stupid vs bug

---
+++ Disemvoweler/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// [02/24/14] Challenge #149 [Easy] Disemvoweler
+
+namespace Disemvoweler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                char[] vowels = new[] { 'a', 'e', 'i', 'o', 'u' };
+                string input = Console.ReadLine();
+                input = input.ToLower();
+
+                string result = "", removedVowels = "";
+
+                foreach (char c in input)
+                {
+                    if (c == ' ')
+                        continue;
+                    else if (vowels.Contains(c))
+                        removedVowels += c;
+                    else
+                        result += c;
+                }
+
+                Console.WriteLine("{0}" + Environment.NewLine + "{1}", result, removedVowels);
+                Console.ReadLine();
+            }
+        }
+    }
+}
