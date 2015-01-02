25cba6d9245e56c0acb0c7f388656ca429097fae
edza <edexdev@gmail.com>
2015-01-02 12:06:06 +0200

    RANDU

--- RANDU/Program.cs
+++ RANDU/Program.cs
@@ -8,8 +8,30 @@ namespace RANDU
 {
     class Program
     {
+        // xn+1 = 65539 · xn (mod 231), with x0 odd.
+        static uint seed = 9991;
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine(Randu0to1().ToString("0.##"));
+                Console.ReadLine();
+            }
+        }
+
+        private static uint Randu()
+        {
+            unchecked
+            {
+                seed = ((uint)65539 * (uint)seed) % (uint)Math.Pow(2, 31);
+            }
+            return seed;
+        }
+
+        private static decimal Randu0to1()
+        {
+            return (decimal)Randu() / (decimal)Math.Pow(2, 31);
         }
     }
 }
